75567fe0991d79b7b2a1e7d4282ba4890e789ac9
Nicholas <contactnicholas81@gmail.com>
2022-01-19 20:34:46 +0000

    prod

--- packages/client/lib/constants.ts
+++ packages/client/lib/constants.ts
@@ -1,4 +1,5 @@
+// Just for now until i setup the api subdomin
 export const LAKRO_API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.lakro.app/v1'
+    ? 'https://jbbuj463vh.execute-api.us-west-2.amazonaws.com/'
     : 'http://localhost:8000/v1';
