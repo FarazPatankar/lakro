f09d13d3e2b5b43b639b65e3bab7ff46bef3587d
Nicholas <contactnicholas81@gmail.com>
2022-01-19 20:37:28 +0000

    bruhh moment

--- packages/client/lib/constants.ts
+++ packages/client/lib/constants.ts
@@ -1,5 +1,5 @@
 // Just for now until i setup the api subdomin
 export const LAKRO_API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://jbbuj463vh.execute-api.us-west-2.amazonaws.com/'
+    ? 'https://jbbuj463vh.execute-api.us-west-2.amazonaws.com/v1'
     : 'http://localhost:8000/v1';
